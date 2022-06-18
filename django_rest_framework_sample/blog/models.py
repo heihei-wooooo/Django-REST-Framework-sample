7a8bdf30358e4db16a1cddea0ee234674e132e18
heihei <raions.seibu@gmail.com>
2022-06-18 12:28:18 +0900

    edit

--- django_rest_framework_sample/blog/models.py
+++ django_rest_framework_sample/blog/models.py
@@ -1,3 +1,20 @@
+from turtle import update
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    mail = models.EmailField()
+
+class Entry(models.Model):
+    STATUS_DRAFT = "draft"
+    STATUS_PUBLIC = "public"
+    STATUS_SET = (
+        (STATUS_DRAFT, "下書き"),
+        (STATUS_PUBLIC, "公開中"),
+    )
+
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
--- django_rest_framework_sample/django_rest_framework_sample/settings.py
+++ django_rest_framework_sample/django_rest_framework_sample/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+u**4phtccjq7w*z9$(=%bntz(nejs98ewj6yb@#!v64cy%oy3'
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
